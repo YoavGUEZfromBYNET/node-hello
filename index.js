abb3dfdb89c33f2de06a2d082687734a23e82b4e
YG <yoav-g@bynet.co.il>
2021-08-22 10:23:37 +0300

    v2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Yoav Container!\n version1'
+  const msg = 'Hello Yoav Container!\n version2'
   res.end(msg);
 });
 
